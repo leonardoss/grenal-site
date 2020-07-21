2ba19ce0bac5e99bfeca440e911fc4e5f3805eda
Leonardo Souza <leonardo.souza@care.com>
2020-07-21 15:09:03 +0200

    add background

--- pages/index.js
+++ pages/index.js
@@ -8,39 +8,94 @@ import moment from 'moment';
 import { useQuery } from '@apollo/react-hooks';
 import MATCHES_QUERY from '../graphql/matches.query';
 
+import MOCK_DATA from '../mock/matches.json';
+
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+const StyledTableRow = withStyles((theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
 export default function Home() {
+  const classes = useStyles();
   // Create a query hook
-  const { data, loading, error } = useQuery(MATCHES_QUERY);
+  // const { data, loading, error } = useQuery(MATCHES_QUERY);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
 
-  if (error) {
-    return <p>Error: {JSON.stringify(error)}</p>;
-  }
+  // if (error) {
+  //   return <p>Error: {JSON.stringify(error)}</p>;
+  // }
 
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <table border="1">
-        <tbody>
-          {data.matches.map((match) => {
-            return (
-              <tr key={`match__${match.id}`}>
-                <td>{match.number}</td>
-                <td>{match.homeTeam}</td>
-                <td>{match.homeScore}</td>
-                <td>{match.awayScore}</td>
-                <td>{match.awayTeam}</td>
-                <td>{moment(match.date).utc().format('DD-MM-YYYY')}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Número</StyledTableCell>
+              <StyledTableCell>Mandante</StyledTableCell>
+              <StyledTableCell>Placar</StyledTableCell>
+              <StyledTableCell>Placar</StyledTableCell>
+              <StyledTableCell>Visitante</StyledTableCell>
+              <StyledTableCell>Data</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {MOCK_DATA.map((match) => (
+              <StyledTableRow key={`match__${match.id}`}>
+                <StyledTableCell>{match.number}</StyledTableCell>
+                <StyledTableCell>{match.homeTeam}</StyledTableCell>
+                <StyledTableCell>{match.homeScore}</StyledTableCell>
+                <StyledTableCell>{match.awayScore}</StyledTableCell>
+                <StyledTableCell>{match.awayTeam}</StyledTableCell>
+                <StyledTableCell>
+                  {moment(match.date).utc().format('DD/MM/YYYY')}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <section className={utilStyles.headingMd}>
         <p>
           <Link href="/teams">
